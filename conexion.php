793b72067b2257462a8286aa4c247061478df9e2
Keren Corral <104870230+KerenKarola@users.noreply.github.com>
2022-05-23 23:28:52 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,20 @@
+<?php
+
+    $host = "127.0.0.1";
+    $user = "u646610080_amorchis";
+    $clave = "Utd_2021";
+    $bd = "u646610080_amorchis";
+   
+
+    $conexion = mysqli_connect($host,$user,$clave,$bd);
+    if (mysqli_connect_errno()){
+        echo "No se pudo conectar a la base de datos";
+        exit();
+    }
+
+    mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
+
+    mysqli_set_charset($conexion,"utf8");
+
+
+?>
